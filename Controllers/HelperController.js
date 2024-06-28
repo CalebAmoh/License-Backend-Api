b3bc679a9bf4dadb44754d94e1f9e425bd7229b5
unionsghr <hr@unionsg.com>
2024-06-28 19:02:01 +0000

    Reactivation
    Added the reactivation of license, and also modified the code to make it more readable and reuseable

--- Controllers/HelperController.js
+++ Controllers/HelperController.js
@@ -1,11 +1,21 @@
 require("dotenv").config();
 const mysql = require("mysql");
+const oracledb = require("oracledb");
+const collect = require("collect.js");
+const util = require("util");
 
-const conn = process.env.connection;
+//mysql connection string
 const host = process.env.host;
 const user = process.env.user;
 const database = process.env.database;
 const password = process.env.password;
+
+//oracle connection strings
+const user_oracle = process.env.DB_USER;
+const password_oracle = process.env.DB_PASSWORD;
+const connectString_oracle = process.env.DB_CONNECTION_STRING;
+const timeout_oracle = process.env.DB_CONNECTION_TIMEOUT;
+const encrypt_key = process.env.ENCRYPT_KEY;
 
 // Create a MySQL connection
 const connection = mysql.createConnection({
@@ -14,6 +24,23 @@ const connection = mysql.createConnection({
 	password: password,
 	database: database
 });
+
+//connect to oracle db
+const connectOracle = async () => {
+	try {
+		//create oracle connection
+		const db = await oracledb.getConnection({
+			user: user_oracle,
+			password: password_oracle,
+			connectString: connectString_oracle,
+			timeout: timeout_oracle
+		});
+
+		return db;
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 // Function to insert data into a MySQL table dynamically
 function insertData(data, tableName, callback) {
@@ -25,6 +52,30 @@ function insertData(data, tableName, callback) {
 
 		// Execute the insert statement
 		connection.query(insertQuery, (error, results, fields) => {
+			if (error) {
+				console.log("Data error !", error);
+				callback({ status: "error", message: error.sqlMessage });
+			} else {
+				console.log("Data inserted successfully!!");
+				callback({ status: "success", message: "data inserted" });
+			}
+		});
+	} catch (error) {
+		console.error("Error adding row:", error);
+		res.status(500).json({ result: "Internal server error", code: "500" });
+	}
+}
+
+// Function to update data MySQL table dynamically
+function updateData(data, tableName, condition, callback) {
+	try {
+		// Generate the insert statement dynamically
+		const columns = Object.keys(data).join(", ");
+		const values = Object.values(data).map(value => `"${value}"`).join(", ");
+		const updateQuery = `UPDATE ${tableName} set ${data} where ${condition}`;
+
+		// Execute the insert statement
+		connection.query(updateQuery, (error, results, fields) => {
 			if (error) {
 				console.log("Data error !", error);
 				callback({ status: "error", message: error.sqlMessage });
@@ -86,9 +137,37 @@ function selectCustomData(query, callback) {
 	}
 }
 
+//this function encrypts data and returns the encrypted value
+const encryptData = async data => {
+	try {
+		//convert object data to array
+		const modified_data = collect(data).toArray();
+
+		//connect to oracle db
+		const db = await connectOracle();
+
+		//bind database connection
+		const execute = util.promisify(db.execute).bind(db);
+
+		//execute the query1
+		let encryption = await execute(
+			`select CBXDMX.pkg_toolkit_modified.fnen('${modified_data}','${encrypt_key}') as encrypted from dual`
+		);
+
+		// console.log(encryption.rows[0][0]);
+		const encrypted_value = Buffer.from(encryption.rows[0][0]).toString("hex");
+
+		return encrypted_value;
+	} catch (error) {
+		console.log(error);
+	}
+};
+
 module.exports = {
 	insertData,
+	updateData,
 	selectDataWithCondition,
 	selectData,
-	selectCustomData
+	selectCustomData,
+	encryptData
 };
--- Controllers/LicenseController.js
+++ Controllers/LicenseController.js
@@ -1,7 +1,9 @@
 const {
 	insertData,
 	selectData,
-	selectCustomData
+	selectCustomData,
+	updateData,
+	encryptData
 } = require("./HelperController");
 const tb_license = "tb_license";
 require("dotenv").config();
@@ -20,10 +22,11 @@ const encrypt_key = process.env.ENCRYPT_KEY;
  * 
  * Activities in {
 	* generateLicense() - for generating license,
+	* getBankDetails() - get bank details for reactivating license
  * }
  ***************************************************************************************************************/
 
-//generate license function1
+//generate license function
 const generateLicense = async (req, res) => {
 	try {
 		//get all data from request
@@ -37,41 +40,51 @@ const generateLicense = async (req, res) => {
 			notification_frequency_id,
 			grace_period
 		} = req.body);
-		
-		const collection = collect(test);
-		const newTest = collection.toArray();
 
-		//once data has been saved encrypt the data
-		const db = await oracledb.getConnection({
-			user: user,
-			password: password,
-			connectString: connectString,
-			timeout: timeout
-		});
+		const testArray = [
+			{ key: "bank_id", value: bank_id },
+			{ key: "license_frequency_id", value: license_frequency_id },
+			{ key: "license_type_id", value: license_type_id },
+			{ key: "start_date", value: start_date },
+			{ key: "end_date", value: end_date },
+			{ key: "notification_start", value: notification_start },
+			{ key: "notification_frequency_id", value: notification_frequency_id },
+			{ key: "grace_period", value: grace_period }
+		];
 
-		const execute = util.promisify(db.execute).bind(db);
+		// const collection = collect(test);
+		// const newTest = collection.toArray();
 
-		//execute the query1
-		let encryption = await execute(
-			`select CBXDMX.pkg_toolkit_modified.fnen('${newTest}','${encrypt_key}') as encrypted from dual`
-		);
+		// //once data has been saved encrypt the data
+		// const db = oracledb.getConnection({
+		// 	user: user,
+		// 	password: password,
+		// 	connectString: connectString,
+		// 	timeout: timeout
+		// });
 
-		// console.log(encryption.rows[0][0]);
-		const encrypted_value = Buffer.from(encryption.rows[0][0]).toString("hex");
-		// console.log(encrypted_value);
-		// console.log(Buffer.from(encrypted_value, "hex"));
-		let ttt = Buffer.from(encrypted_value, "hex");
+		// const execute = util.promisify(db.execute).bind(db);
 
-		let encryption1 = await execute(
-			`select CBXDMX.pkg_toolkit_modified.fnde('${encrypted_value}','${encrypt_key}') as encrypted from dual`
-		);
+		// //execute the query1
+		// let encryption = await execute(
+		// 	`select CBXDMX.pkg_toolkit_modified.fnen('${newTest}','${encrypt_key}') as encrypted from dual`
+		// );
 
-		// Log the raw data to see what it contains1
-		// console.log("Raw query result:", encryption1);
-		console.log("Raw encrypted value:", encryption1.rows[0]);
-		const resultString = encryption1.rows[0][0];
-		console.log(resultString)
-		
+		// // console.log(encryption.rows[0][0]);
+		// const encrypted_value = Buffer.from(encryption.rows[0][0]).toString("hex");
+
+		//encrypt data
+		const encrypted_value = encryptData(testArray);
+
+		// let encryption1 = await execute(
+		// 	`select CBXDMX.pkg_toolkit_modified.fnde('${encrypted_value}','${encrypt_key}') as encrypted from dual`
+		// );
+
+		// // Log the raw data to see what it contains1
+		// // console.log("Raw query result:", encryption1);
+		// console.log("Raw encrypted value:", encryption1.rows[0]);
+		// const resultString = encryption1.rows[0][0];
+		// console.log(resultString);
 
 		/**
 			* insert data into table
@@ -86,7 +99,8 @@ const generateLicense = async (req, res) => {
 				end_date,
 				notification_start,
 				notification_frequency_id,
-				grace_period
+				grace_period,
+				encrypted_value
 			},
 			tb_license,
 			result => {
@@ -111,11 +125,7 @@ const generateLicense = async (req, res) => {
 	}
 };
 
-const decrypt = async (req, res) => {
-	const { data } = req.body;
-};
-
-//reactivate license
+//get bank details for reactivating license
 const getBankDetails = async (req, res) => {
 	try {
 		//select the current license details of the bank
@@ -137,7 +147,86 @@ const getBankDetails = async (req, res) => {
 	}
 };
 
+//reactivate license
+const reactivateLicense = async (req, res) => {
+	try {
+		//get all data from request
+		const license_details = ({
+			bank_id,
+			license_frequency_id,
+			license_type_id,
+			start_date,
+			end_date,
+			notification_start,
+			notification_frequency_id,
+			grace_period
+		} = req.body);
+
+		//encrypt data
+		const encrypted_value = encryptData(license_details);
+
+		if (encrypted_value != NULL) {
+			//update the expiry status of the already existing bank to expired
+			//select condition
+			const condition = `bank_id = ${bank_id}`;
+
+			//update column and values
+			const data = (expired_status = 1);
+
+			//update table
+			updateData(data, tb_license, condition, result => {
+				//return a success message if insertion is successful else error message
+				if (result.status === "success") {
+					//now insert a new bank record
+					/**
+						* insert data into table
+						* helper.insertData(@dataParam->the data to be inserted, @tableParam->table to insert data, @result->result from insert)
+            */
+					insertData(
+						{
+							bank_id,
+							license_frequency_id,
+							license_type_id,
+							start_date,
+							end_date,
+							notification_start,
+							notification_frequency_id,
+							grace_period,
+							encrypted_value
+						},
+						tb_license,
+						result => {
+							//return a success message if insertion is successful else error message
+							if (result.status === "success") {
+								res.status(200).json({
+									result: "Encryption successful",
+									data: encrypted_value,
+									code: "000"
+								});
+							} else {
+								res.status(300).json({
+									result: "An error occured",
+									code: "300"
+								});
+							}
+						}
+					);
+				} else {
+					res.status(300).json({
+						result: "An error occured",
+						code: "300"
+					});
+				}
+			});
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ status: "500", result: "Contact admin" });
+	}
+};
+
 module.exports = {
 	generateLicense,
-	getBankDetails
+	getBankDetails,
+	reactivateLicense
 };
